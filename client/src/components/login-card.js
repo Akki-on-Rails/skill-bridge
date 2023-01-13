d47f59c0b5945df21adb2aea11cdca57b3bd9c74
Frank Gerngross <f.gerngross@googlemail.com>
2023-01-13 12:07:42 +0100

    fixed bugs with the styles and added a background to the index

--- client/src/components/login-card.js
+++ client/src/components/login-card.js
@@ -1,31 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import './login-card.module.scss';
+import './login-card.module.css';
 
 const LoginCard = () => {
   return (
     <div className='card'>
-      {/* here we should insert the Logo: */}
-      <div class='logo'>
-        {/* <img src="avatar.png" /> */}
+      <div className="login">
+        {/* <div class="avatar">
+          <img src="avatar.png" />
+        </div> */}
+        <h2>Login</h2>
+        <h3>Welcome back Kelly</h3>
+
+        <form className="login-form">
+          <div className="textbox">
+            <input type="email" placeholder="Username" />
+            {/* <span className="material-symbols-outlined"> account_circle </span> */}
+          </div>
+          <div className="textbox">
+            <input type="password" placeholder="Password" />
+            {/* <span className="material-symbols-outlined"> lock </span> */}
+          </div>
+          <button type="submit">LOGIN</button>
+          <a href="https://website.com">Forgot your password?</a>
+        </form>
       </div>
-      <h2>Login</h2>
-      <h3>Welcome! Please enter your login data.</h3>
-      {/* I guess we need to edit the form and make it 'REACTish' */}
-      <form className='form'>
-        <div className='textbox'>
-          <input type="email" placeholder="Username" />
-          {/* <span class={loginCardStyles.material-symbols-outlined}> account_circle </span> */}
-        </div>
-        <div className='textbox'>
-          <input type="password" placeholder="Password" />
-          {/* <span className={loginCardStyles.material-symbols-outlined}> lock </span> */}
-        </div>
-        <button type="submit">LOGIN</button>
-        {/* Here we need a valid link for resetting the password: */}
-        <Link to='/'>Forgot your password?</Link>
-      </form>
     </div>
   )
 }
