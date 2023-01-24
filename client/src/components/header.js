0f8c90ac6873ee7f226a30b46c87772b30f9de86
Frank Gerngross <f.gerngross@googlemail.com>
2023-01-24 12:05:22 +0100

    optimized styles for the header menu and added avatar and log out button

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 import styles from './header.module.scss';
+import avatars from '../styles/avatars.module.scss';
+import buttons from '../styles/buttons.module.scss';
+
 import "../assets/css/fontawesome.css"
 import "../assets/css/brands.css"
 import "../assets/css/solid.css"
@@ -20,7 +23,14 @@ const Header = () => {
         <a href="#">contact</a>
       </div>
       <div className={styles.login}>
-
+        {/* here we need to insert user images dynamicly from DB */}
+        {/* also we have to create a conditional: */}
+        {/* if logged in show avatar and 'log out' */}
+        {/* else show 'log in' and 'sign up' */}
+        <div className={styles.user_comp}>
+          <a className={buttons.btn_flat}>Log out</a>
+          <img className={avatars.avatar_small} src='https://picsum.photos/id/64/200/300' alt='user avatar'></img>
+        </div>
       </div>
     </header>
   )
