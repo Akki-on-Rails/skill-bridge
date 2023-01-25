c22072b24095e42d3bf2d7880da1f906ee87ab6e
JLBSoftware <98103461+JLBSoftware@users.noreply.github.com>
2023-01-25 15:47:45 +0100

    bullet-list

---
+++ client/src/components/bullet-list.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./bullet-list.css";
+
+
+function BulletListFeatures () {
+  return (
+    <div className='componentLevel'>
+      <h2>Job Search Features</h2>
+      <div className='parentStyle'>
+      <ul >
+        <li className='elementStyle'>Feature 1</li>
+        <li className='elementStyle'>Feature 2</li>
+        <li className='elementStyle'>Feature 3</li>
+      </ul>
+      <ul>
+        <li className='elementStyle'>Feature 4</li>
+        <li className='elementStyle'>Feature 5</li>
+        <li className='elementStyle'>feature 6</li>
+      </ul>
+      </div>
+    </div>
+  );
+};
+
+export default BulletListFeatures;
