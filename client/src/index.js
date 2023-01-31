249916e37eb1f2e7c7bb329d2d0f580ba5f3d9e0
Ihab <ihab.mortada1@gmail.com>
2023-01-31 16:31:08 +0100

    removing-unecessary-files

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import './index.scss';
 import LoginCard from './components/login-card';
 import Footer from './components/footer';
 import Header from './components/header';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,7 +18,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
