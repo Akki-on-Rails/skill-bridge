8e7d754a26459897aa35d33d736cae4dc09e2d98
JLBSoftware <98103461+JLBSoftware@users.noreply.github.com>
2023-02-01 17:23:29 +0100

    neuer slider mit installationen

--- client/src/components/image-slider.js
+++ client/src/components/image-slider.js
@@ -3,6 +3,8 @@ import "./image-slider.css";
 import testbild1 from '../images/testbildx.jpg';
 import testbild2 from '../images/testfeatures.jpg';
 import testbild3 from '../images/testbildy.jpg';
+import Flickity from "react-flickity-component";
+import "./image-slider.css";
 
 const images = [testbild1,testbild2, testbild3]
 
@@ -14,18 +16,14 @@ function ImageComponent() {
   };
 
   return (
-    <div style={{display:"flex", flexDirection:"row", justifyContent: "center", padding:"5vh"}}>
-      <div class="slider">
-          <div class="slides">
-          <div id="slide-1"><img src={testbild1} style={{width:"100%"}}/></div>
-          <div id="slide-2"><img src={testbild2} style={{width:"100%"}}/></div>
-          <div id="slide-3"><img src={testbild3} style={{width:"100%"}}/></div>
-        
-        </div>
-        <a href="#slide-1"><img src={testbild1} style={{width:"100%"}}/></a>
-        <a href="#slide-2"><img src={testbild2} style={{width:"100%"}}/></a>
-        <a href="#slide-3"><img src={testbild3} style={{width:"100%"}}/></a>
-      </div>
+    <div className='bulletDiv'>
+
+    <Flickity>
+      <img src={testbild1} />
+      <img src={testbild2} />
+      <img src={testbild3} />
+    </Flickity>
+
    
     </div>
   )
