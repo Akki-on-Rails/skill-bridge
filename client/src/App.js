9a875fe8a0c76af3c555e9050aeac04e5ea9083c
Frank Gerngross <f.gerngross@googlemail.com>
2023-02-03 10:37:42 +0100

    trying to add media query to info-block in home.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,29 +14,30 @@ import styles from "./styles/App.scss"
 function App() {
   const { user } = useAuthContext()
   return (
-    <div className={styles.body_wrapper}>
+    <div className={styles.content_wrapper}>
       <BrowserRouter>
-        <Navbar />
-        <div className="pages">
-          <Routes>
-            <Route
-              path="/"
-              element={<Home />}
-              // element={user ? <Home /> : <Navigate to="/login" />}
-            />
-            <Route
-              path="/login"
-              element={!user ? <Login /> : <Navigate to="/" />}
-            />
+        <div className={styles.pages_wrapper}>
+          <Navbar />
+          <div className={styles.pages}>
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+                // element={user ? <Home /> : <Navigate to="/login" />}
+                />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+                />
 
-            <Route
-              path="/signup"
-              element={!user ? <Signup /> : <Navigate to="/" />}
-            />
-          </Routes>
+              <Route
+                path="/signup"
+                element={!user ? <Signup /> : <Navigate to="/" />}
+                />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-
-        <Footer />
       </BrowserRouter>
     </div>
   )
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,14 +7,19 @@ import IntroBlock from "../components/intro-block"
 import ImageSlider from "../components/image-slider"
 import BulletList from "../components/bullet-list"
 
-const Home = () => {
+const screenWidth = document.documentElement.clientWidth || window.innerWidth;
+const doubleBlockStyles = () => {
+  return (
+    screenWidth <= 700 ? { display: 'block' } : { display: 'flex' }
+  )
+}
+
+function Home() {
   return (
     <body>
-      <div className={styles.double_block}>
+      <div style={doubleBlockStyles}>
         <IntroBlock />
         <IntroBlock />
-        <div className={styles.left_item}></div>
-        <div className={styles.right_item}></div>
       </div>
       <ImageSlider />
       <BulletList />
