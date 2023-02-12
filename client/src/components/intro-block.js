1cd81aa6f68cb39b75c52b9f0d66a03e40ec656a
Frank Gerngross <f.gerngross@googlemail.com>
2023-02-12 19:51:13 +0100

    added image to intro block

--- client/src/components/intro-block.js
+++ client/src/components/intro-block.js
@@ -6,12 +6,18 @@ import buttons from "../styles/buttons.module.scss"
 import "../assets/css/fontawesome.css"
 import "../assets/css/brands.css"
 import "../assets/css/solid.css"
+import image from '../images/casual-life-3d-young-woman-imagining-things-while-reading-e-book.png'
 
 const IntroBlock = () => {
   return (
     <div className={styles.block_wrapper}>
-      <h2>Find the apprenticeship that fits your skillset, not just your grades.</h2>
-      <a href="#" className={buttons.btn_flat}>&emsp;Sign up for free&emsp;</a>
+      <div className={styles.text_wrapper}>
+        <h2>Find the apprenticeship that fits your skillset, not just your grades.</h2>
+        <a href="#" className={buttons.btn_flat}>&emsp;Sign up for free&emsp;</a>
+      </div>
+      {/* Illustration by <a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">Icons 8</a> from <a href="https://icons8.com/illustrations">Ouch!</a> */}
+      {/* Illustration by <a href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">Icons 8</a> from <a href="https://icons8.com/illustrations">Ouch!</a> */}
+      <img src={image}></img>
     </div>
   )
 }
