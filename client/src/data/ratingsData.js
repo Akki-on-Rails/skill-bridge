854b4ccf8109a942ab56ae3346b0745c37237726
Frank Gerngross <f.gerngross@googlemail.com>
2023-02-21 15:33:50 +0100

    review carousel is running with correct content. Styling is still messy.

--- client/src/data/ratingsData.js
+++ client/src/data/ratingsData.js
@@ -1,30 +1,39 @@
 export const ratingsData = [
   {
-    id: 1,
-    name: "Sarah",
-    title: "Software Engineer",
-    rating: 5,
-    comment: "I never thought I'd find a job in tech, but Skillbridge changed that for me. The website helped me discover my hidden tech skills and now I'm working as a Software Engineer. I'm so happy!"
+    user: {
+      id: 1,
+      name: "Sarah",
+      title: "Software Engineer",
+      rating: 5,
+      comment: "I never thought I'd find a job in tech, but Skillbridge changed that for me. The website helped me discover my hidden tech skills and now I'm working as a Software Engineer. I'm so happy!"
+    },
+
   },
   {
-    id: 2,
-    name: "Alexander",
-    title: "Graphic Design",
-    rating: 4,
-    comment: "I found my dream job as a Graphic Designer thanks to SkillBridge. The skill assessment test was easy to take and the website was easy to use. I highly recommend it!"
+    user: {
+      id: 2,
+      name: "Alexander",
+      title: "Graphic Design",
+      rating: 4,
+      comment: "I found my dream job as a Graphic Designer thanks to SkillBridge. The skill assessment test was easy to take and the website was easy to use. I highly recommend it!"
+    },
   },
   {
-    id: 3,
-    name: "Emma",
-    title: "Customer Service",
-    rating: 4,
-    comment: "I wasn't sure what job to look for, but Skillbridge helped me find my way. I took the skill assessment test and was matched with a job in customer service. I never thought I'd enjoy this field, but I do! Thanks, SkillBridge!"
+    user: {
+      id: 3,
+      name: "Emma",
+      title: "Customer Service",
+      rating: 4,
+      comment: "I wasn't sure what job to look for, but Skillbridge helped me find my way. I took the skill assessment test and was matched with a job in customer service. I never thought I'd enjoy this field, but I do! Thanks, SkillBridge!"
+    },
   },
   {
-    id: 4,
-    name: "Tom",
-    title: "Marketing",
-    rating: 5,
-    comment: "SkillBridge helped me find the perfect marketing internship that matched my skills and interests. I gained valuable experience and built my portfolio, and now I have a job in my dream field. Thank you SkillBridge!",
+    user: {
+      id: 4,
+      name: "Tom",
+      title: "Marketing",
+      rating: 5,
+      comment: "SkillBridge helped me find the perfect marketing internship that matched my skills and interests. I gained valuable experience and built my portfolio, and now I have a job in my dream field. Thank you SkillBridge!",
+    },
   },
 ]
