bd5ea0045ce7e9901bf8f0877fba22b77493b832
Frank Gerngross <f.gerngross@googlemail.com>
2023-02-25 11:50:03 +0100

    new styles for the bullet list

--- client/src/components/bullet-list.js
+++ client/src/components/bullet-list.js
@@ -7,42 +7,43 @@ const BulletListFeatures = () => {
   return (
     <div className={styles.bullet_wrapper}>
       <h2>What sets us apart</h2>
-      <div className={styles.bullet_one}>
-        <ul className={styles.bullet_head}>
-          <li>Skill Matching</li>
-          <li>Matches based on you</li>
-        </ul>
-        <p>Our platform matches you with
-          the right job based on your skills
-          and interests. No more browsing
-          through irrelevant job postings</p>
-      </div>
-      <div className={styles.bullet_two}>
-        <ul className={styles.bullet_head}>
-          <li>Career Growth</li>
-          <li>Skill & Network</li>
-        </ul>
-        <p>Our platform is designed to help
-          you build your skills and
-          network, setting you up for
-          success in your future career.
-        </p>
-      </div>
-      <div className={styles.bullet_three}>
-        <ul className={styles.bullet_head}>
-          <li>Skill Assessment</li>
-          <li>Identity strengths</li>
-        </ul>
-        <p>Take our assessment to discover
-          your strengths and interests and
-          see which careers may be a
-          good fit for you.
-        </p>
-      </div>
-      <div className={styles.bullet_four}>
+      <div className={styles.bullet_container}>
+        <div className={styles.bullet_element}>
+          <ul className={styles.bullet_head}>
+            <li>Skill Matching</li>
+            <li><span>Matches based on you</span></li>
+          </ul>
+          <p>Our platform matches you with
+            the right job based on your skills
+            and interests. No more browsing
+            through irrelevant job postings</p>
+        </div>
+        <div className={styles.bullet_element}>
+          <ul className={styles.bullet_head}>
+            <li>Career Growth</li>
+            <li><span>Skill & Network</span></li>
+          </ul>
+          <p>Our platform is designed to help
+            you build your skills and
+            network, setting you up for
+            success in your future career.
+          </p>
+        </div>
+        <div className={styles.bullet_element}>
+          <ul className={styles.bullet_head}>
+            <li>Skill Assessment</li>
+            <li><span>Identity strengths</span></li>
+          </ul>
+          <p>Take our assessment to discover
+            your strengths and interests and
+            see which careers may be a
+            good fit for you.
+          </p>
+        </div>
+      <div className={styles.bullet_element}>
         <ul className={styles.bullet_head}>
           <li>Bias-Free Hiring</li>
-          <li>Skills over Grades</li>
+          <li><span>Skills over Grades</span></li>
         </ul>
         <p>Our focus on skills over grades
           means you're evaluated based
@@ -50,16 +51,17 @@ const BulletListFeatures = () => {
           what grades you got.
         </p>
       </div>
-      <div className={styles.bullet_five}>
+      <div className={styles.bullet_element}>
         <ul className={styles.bullet_head}>
           <li>Goal Oriented</li>
-          <li>Motivation & Passions</li>
+          <li><span>Motivation & Passions</span></li>
         </ul>
         <p>By understanding your
           motivations and passions,
           SkillBridge can help you find a
           job that aligns with your goals.
         </p>
+      </div>
       </div>
     </div>
 
