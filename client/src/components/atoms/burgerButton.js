67b5ab60457411df86cf010b98f1bf90c6b6b8e3
Frank Gerngross <f.gerngross@googlemail.com>
2023-03-04 14:31:44 +0100

    deleted some unused files

--- client/src/components/atoms/burgerButton.js
+++ client/src/components/atoms/burgerButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styles from './burgerButton.module.scss';
+import styles from '../../styles/atoms/burgerButton.module.scss';
 
 const BurgerButton = () => {
   return (
